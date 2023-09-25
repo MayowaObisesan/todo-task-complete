f003af091ad599877b359cc337260d95a07f9ac1
MayowaObisesan <mayowaobi74@gmail.com>
2023-09-25 18:09:36 +0100

    Update

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -6,31 +6,35 @@ export const Todos = () => {
     const { todos } = useContext(TodoContext);
     return (
         <ul>
-            {!!todos.length && todos.map(
+            {!!todos.length ? todos.map(
                 todo => <TodoItem todo={todo} key={todo.id} />
-            )}
+            ) : <div className={"w-full h-20 text-3xl py-24 px-8 text-gray-400"}>No Todo Found</div>}
         </ul>
     );
 }
 
 export const CompletedTodos = () => {
     const { todos } = useContext(TodoContext);
+    const completedTodos = todos.filter(todo => todo.completed);
+
     return (
         <ul>
-            {!!todos.length && todos.filter(todo => todo.completed).map(
+            {!!todos.length ? completedTodos.map(
                 todo => <TodoItem todo={todo} key={todo.id} />
-            )}
+            ) : <div className={"w-full h-20 text-3xl py-24 text-center px-8 text-gray-400"}>No Completed Todo Found</div>}
         </ul>
     );
 }
 
 export const UncompletedTodos = () => {
     const { todos } = useContext(TodoContext);
+    const uncompletedTodos = todos.filter(todo => !todo.completed);
+
     return (
         <ul>
-            {!!todos.length ? todos.filter(todo => !todo.completed).map(
+            {!!uncompletedTodos.length ? todos.filter(todo => !todo.completed).map(
                 todo => <TodoItem todo={todo} key={todo.id} />
-            ) : <div className={"w-full h-60 text-3xl py-64 px-8 text-gray-400"}>No Todo Found</div>}
+            ) : <div className={"w-full h-20 text-3xl py-24 px-8 text-gray-400"}>No Uncompleted Todo Found</div>}
         </ul>
     );
 }
